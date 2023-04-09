32a8ba3c3007ce35a620018e1adae26e571b96b3
dekkanihalima <dekkanihalima9@gmail.com>
2023-04-09 14:53:28 -0700

    libmy a

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -4,17 +4,20 @@
  * _strncat - concatenates n bytes from a string to another
  * @dest: destination string
  * @src: source string
- * @n: number of bytes of str to the resulting string dest
+ * @n: number of bytes of str to concatenate
+ *
+ * Return: a pointer to the resulting string dest
  */
-char *_strnact(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
 	i = 0;
 	j = 0;
 
-	while (dest([i] != '\0')
+	while (dest[i] != '\0')
 		i++;
+
 	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
