ef76f1bfa0812f5c0b420dabe6647a149a272d24
root <halimadekkani9@gmain.com>
2023-05-08 16:31:49 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,19 +12,19 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, wt, l;
+	int file, wt, i=0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (l = 0; text_content[l];)
-			l++;
+		for (i = 0; text_content[i];)
+			i++;
 	}
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wt = write(file, text_content, l);
+	wt = write(file, text_content, i);
 
 	if (file == -1 || wt == -1)
 		return (-1);
