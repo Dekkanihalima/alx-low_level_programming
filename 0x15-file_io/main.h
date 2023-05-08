d10d37e1d401aa7ccba49f6b339fd4564ea5547f
root <halimadekkani9@gmain.com>
2023-05-08 16:24:28 -0700

    task 1

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef FILE_IO
-#define FILE_IO
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -8,6 +8,7 @@
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
 
